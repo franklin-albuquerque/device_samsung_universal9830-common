f012a7368643be96ece88e26fd6aeac3c164f1c4
Francescodario Cuzzocrea <bosconovic@gmail.com>
2024-02-12 14:28:48 +0100

    universal9830: import soundtrigger hal
    Change-Id: Iad85e4fb96e727e9dbe36fdce5c27fa10ea64e69
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -29,6 +29,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.soundtrigger@2.0-impl:32 \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
@@ -49,6 +50,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 TARGET_EXCLUDES_AUDIOFX := true
 
